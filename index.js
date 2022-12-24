87eb83f86d032b357fd1ef7061f9ee6b929e8e66
Sarah Al-Dhaferi <55980263+SarahSalem-hub@users.noreply.github.com>
2022-12-24 23:40:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+function saturdayFun(string = "roller-skate"){
+    return `This Saturday, I want to ${string}!`
+}
+
+function mondayWork(string = "go to the office"){
+    return`This Monday, I will ${string}.`
+}
+
+function wrapAdjective(visualFlair = "*"){
+
+    
+    return function(string = "special"){
+        return `You are ${visualFlair}${string}${visualFlair}!`
+    }
+    
+}
+const encouragingPromptFunction = wrapAdjective("!!!")
+console.log(wrapAdjective("%")("a dedicated programmer"))
